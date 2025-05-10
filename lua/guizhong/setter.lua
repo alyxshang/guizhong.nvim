29a5e421bd11d4b5805cf273a5b714a935ce2a4b
alyxshang <179976644+alyxshang@users.noreply.github.com>
2025-05-11 00:21:16 +0200

    v.0.2.0

--- lua/guizhong/setter.lua
+++ lua/guizhong/setter.lua
@@ -15,57 +15,78 @@ local gui = require("guizhong/gui")
 
 -- Importing the function
 -- for setting the highlight
--- groups for "Noice" elements.
-local noice = require("guizhong/noice")
-
--- Importing the function
--- for setting the highlight
 -- groups for the builtin
 -- syntax-highlighting groups.
 local syntax = require("guizhong/syntax")
-
--- Importing the function
--- for setting the highlight
--- groups for "NvimTree"
--- elements.
-local nvimTree = require("guizhong/nvimtree")
-
--- Importing the functions
--- for setting the highlight
--- groups for "BufferLines"
--- elements.
-local bufferlines = require("guizhong/buffers")
-
--- Importing the function
--- for setting the highlight
--- groups for "Telescope"
--- elements.
-local telescope = require("guizhong/telescope")
-
--- Importing the function
--- for setting the highlight
--- groups for "TreeSitter"
--- elements.
-local treesitter = require("guizhong/treesitter")
 
 -- A function to call all imported
 -- functions and set the different
 -- highlight groups using the 
 -- supplied color palette.
 function setHighlights(palette)
-  noice.setNoiceGroups(palette)
+
+  if pcall(require, "noice") then
+    
+    -- Importing the function
+    -- for setting the highlight
+    -- groups for "Noice" elements.
+    local noice = require("guizhong/noice")
+    noice.setNoiceGroups(palette)
+
+  end
+
+  if pcall(require, "nvim-tree") then
+    
+    -- Importing the function
+    -- for setting the highlight
+    -- groups for "NvimTree"
+    -- elements.
+    local nvimTree = require("guizhong/nvimtree")
+    nvimTree.setNvimTreeGroups(palette)
+
+  end
+
+  if pcall(require, "telescope") then
+    
+    -- Importing the function
+    -- for setting the highlight
+    -- groups for "Telescope"
+    -- elements.
+    local telescope = require("guizhong/telescope")
+    telescope.setTelescopeGroups(palette)
+
+  end
+
+  if pcall(require, "treesitter") then
+    
+    -- Importing the function
+    -- for setting the highlight
+    -- groups for "TreeSitter"
+    -- elements.
+    local treesitter = require("guizhong/treesitter")
+    treesitter.setTreeSitterGroups(palette)
+
+  end
+
+  if pcall(require, "bufferline") then
+    
+    -- Importing the functions
+    -- for setting the highlight
+    -- groups for "BufferLines"
+    -- elements.
+    local bufferlines = require("guizhong/buffers")
+
+    if palette.transparent then
+      bufferlines.setBufferLinesGroupsTransparent(palette)
+    else
+      bufferlines.setBufferLinesGroups(palette)
+    end
+
+  end
+
   lsp.setBuiltinLSPGroups(palette)
   gui.setBuiltinGUIGroups(palette)
-  nvimTree.setNvimTreeGroups(palette)
-  telescope.setTelescopeGroups(palette)
-  syntax.setBuiltinSyntaxGroups(palette)
-  treesitter.setTreeSitterGroups(palette)
-  if palette.transparent then
-    bufferlines.setBufferLinesGroupsTransparent(palette)
-  else
-    bufferlines.setBufferLinesGroups(palette)
-  end
-end
+  syntax.setBuiltinSyntaxGroups(palette)end
 
 -- Exporting the function to
 -- set all important highlight
