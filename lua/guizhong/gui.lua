dad1aba109b2008674994fa75a28ce9a62d1f55d
🖤 𝓐𝓛𝓨𝓧 - 天权🖤🐉 <179976644+alyxshang@users.noreply.github.com>
2025-05-21 12:13:40 +0200

    Update gui.lua

--- lua/guizhong/gui.lua
+++ lua/guizhong/gui.lua
@@ -10,10 +10,6 @@ function setBuiltinGUIGroups(palette)
   -- support transparency.
   if palette.transparent then
     vim.api.nvim_set_hl(
-      0,
-      'EndOfBuffer',
-      { fg = 'NONE' }) 
-    vim.api.nvim_set_hl(
       0, 
       'Normal', 
       { fg = palette.fg, bg = 'NONE' })
